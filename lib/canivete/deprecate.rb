023023244d57e7a9d1cedc39c115af0a148ce04c
Diego Carrion <dc.rec1@gmail.com>
2010-04-25 04:14:51 +0800

    refactors

--- lib/canivete/deprecate.rb
+++ lib/canivete/deprecate.rb
@@ -7,11 +7,10 @@ module Canivete
 		end
 
 		def method_added(method_name)
-			if self.instance_variable_get(:@_deprecate_it) != nil
-				puts "#{method_name} will be redefined"
-				self.instance_variable_set(:@_deprecate_it, nil)
+			unless @_deprecation_options.nil?
         msg = "Warning: calling deprecated method #{self}.#{method_name}."
         msg += "Use #{self}##{@_deprecation_options[:for]}" unless @_deprecation_options[:for].nil?
+				@_deprecation_options = nil
 				module_eval <<END
 					alias_method :deprecated_#{method_name}, :#{method_name}
 					def #{method_name}(*args, &block)
@@ -24,7 +23,6 @@ END
 		end
 
 		def deprecate(opts = {})
-			self.instance_variable_set :@_deprecate_it, true
       @_deprecation_options = opts
 		end
 
