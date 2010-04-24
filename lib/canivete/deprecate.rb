baef210fcc12256b208305961bda34e0dc85f837
Diego Carrion <dc.rec1@gmail.com>
2010-04-25 04:15:18 +0800

    changed tabs in favor of spaces

--- lib/canivete/deprecate.rb
+++ lib/canivete/deprecate.rb
@@ -1,30 +1,30 @@
 module Canivete
 
-	module Deprecate
+  module Deprecate
 
-		def self.included(base)
-			base.extend(Deprecate)
-		end
+    def self.included(base)
+      base.extend(Deprecate)
+    end
 
-		def method_added(method_name)
-			unless @_deprecation_options.nil?
+    def method_added(method_name)
+      unless @_deprecation_options.nil?
         msg = "Warning: calling deprecated method #{self}.#{method_name}."
         msg += "Use #{self}##{@_deprecation_options[:for]}" unless @_deprecation_options[:for].nil?
-				@_deprecation_options = nil
-				module_eval <<END
-					alias_method :deprecated_#{method_name}, :#{method_name}
-					def #{method_name}(*args, &block)
-						warn "#{msg}"
-						deprecated_#{method_name}(*args, &block)
-					end
-END
-			end
-			super
-		end
+        @_deprecation_options = nil
+        module_eval <<END
+          alias_method :deprecated_#{method_name}, :#{method_name}
+          def #{method_name}(*args, &block)
+            warn "#{msg}"
+            deprecated_#{method_name}(*args, &block)
+          end
+          END
+      end
+      super
+    end
 
-		def deprecate(opts = {})
+    def deprecate(opts = {})
       @_deprecation_options = opts
-		end
+    end
 
-	end
+  end
 end
--- spec/canivete/deprecate_spec.rb
+++ spec/canivete/deprecate_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe Canivete::Deprecate do
 
-	before(:each) do
-		@test = Test.new
-	end
+  before(:each) do
+    @test = Test.new
+  end
 
-	it "should warn when a deprecated method is called" do
-		@test.should_receive(:warn)
-		@test.test
-	end
+  it "should warn when a deprecated method is called" do
+    @test.should_receive(:warn)
+    @test.test
+  end
 
-	it "should not warn when a ordinary method is called" do
-		@test.should_not_receive(:warn)
-		@test.test2
-	end
+  it "should not warn when a ordinary method is called" do
+    @test.should_not_receive(:warn)
+    @test.test2
+  end
 
   it "should accept a :for parameter and used it to warn in favor of which method the first was deprecated" do
     @test.should_receive(:warn) do |message|
@@ -26,16 +26,16 @@ end
 
 # test class below
 class Test
-	include Canivete::Deprecate
+  include Canivete::Deprecate
 
-	deprecate
-	def test
+  deprecate
+  def test
 
-	end
+  end
 
-	def test2
+  def test2
 
-	end
+  end
 
   deprecate :for => :test
   def test3
